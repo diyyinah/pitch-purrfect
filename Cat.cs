3a687ce44a0b92b9ca395c28eca3ee9931d4fa59
Diyyinah Jamora <diyyinah@gmail.com>
2019-01-27 13:42:48 -0500

    Add files via upload
    These are all the Pitch Purrfect scripts to date. First upload to Github! Sadly I locked myself out of Sourcetree because I couldn't remember my password so here I am using the web version. Womp womp womp. Let's hope this works just as well. :)

---
+++ Cat.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+
+    public float maxPitch = 500f;
+    public float maxHeight = 5.5f;
+    // public float upForce = 200f;
+    //
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    public pitchv2 PitchFetcher;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      if (isDead == false)
+      {
+        // transform.position = new Vector3(0, PitchFetcher.pitchValue / 120 - 2, 0);
+        float normalizedPitch = PitchFetcher.pitchValue / maxPitch;
+        float height = normalizedPitch * maxHeight;
+        transform.localPosition = new Vector3(0,height,0);
+        Debug.Log(PitchFetcher.pitchValue + " " + height);
+      }
+      // Set maximum to 0, min to 5.5
+
+        //if (isDead == false)
+        //{
+          //if (Input.GetMouseButtonDown(0)) {
+            //rb2d.velocity = Vector2.zero;
+            // If player is not dead and has clicked the button,
+            // Means player will be rising because the player has flapped,
+            // Or falling because they're affected by gravity
+            //rb2d.AddForce(new Vector2(0, upForce));
+            // x has 0 units of force because it's the world moving
+          //}
+        //}
+
+        void OnCollisionEnter2D () //this is Unity API, will be looking for this
+        {
+          isDead = true;
+          GameController.instance.CatDied ();
+          // once it's true, can't flap anymore because check will fail
+        }
+    }
+}
---
+++ ScrollingObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Added Rigidbody2D to the ground and set it to Kinematic so only affected by script
+
+public class ScrollingObject : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+        //make reference to rb2d
+        //Checked for rb2d attached to GO script is attachedto and store reference in variable
+        rb2d.velocity = new Vector2 (GameController.instance.scrollSpeed, 0);
+        // set velocity.  moving 2 pieces of ground and column at same speed
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameController.instance.gameOver == true)
+        {
+          rb2d.velocity = Vector2.zero;
+          // stop any and all objects scrolling when game control says game over
+        }
+    }
+}
